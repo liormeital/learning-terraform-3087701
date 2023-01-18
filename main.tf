42fa5f7204f8a914c437d848048a52d539f82a2c
liormeital <122827432+liormeital@users.noreply.github.com>
2023-01-18 15:42:27 +0200

    Update SG

--- main.tf
+++ main.tf
@@ -14,6 +14,41 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
+resource "aws_security_group" "WebAppSG" {
+  name        = "allow_http"
+  tags  =  {
+    Terraform = "true"
+  }
+  vpc_id = data.aws_vpc.default.id
+}
+
+resource "aws_security_group_rule" "http_in" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.WebAppSG.id
+}
+
+resource "aws_security_group_rule" "https_in" {
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.WebAppSG.id
+}
+
+resource "aws_security_group_rule" "all_out" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.WebAppSG.id
+}
+
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
