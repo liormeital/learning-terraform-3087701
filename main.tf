6d1503ef5666f0f31c44b0f987cd987579e12931
liormeital <122827432+liormeital@users.noreply.github.com>
2023-01-19 22:02:02 +0200

    Prepare to Scale

--- main.tf
+++ main.tf
@@ -14,8 +14,22 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-data "aws_vpc" "default" {
-  default = true
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
 }
 
 module "WebApp_sg" {
@@ -24,7 +38,7 @@ module "WebApp_sg" {
 
   name           = "WebApp_sg"
   description    = "Security group for WebApp_sg"
-  vpc_id         = data.aws_vpc.default.id
+  vpc_id         = module.vpc.public_subnets[0]
 
   ingress_rules  =  ["https-443-tcp","http-80-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
